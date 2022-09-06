576ea343ec2f5d649ca858352f5f8d5b297a7e6c
Erik S. Carlsten <ecarlste@gmail.com>
2022-09-06 14:02:14 -0600

    Added a decorator for the custom serializer

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,6 +1,15 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { map, Observable } from 'rxjs';
+
+export function Serialize(dto: any) {
+  return UseInterceptors(new SerializerInterceptor(dto));
+}
 
 export class SerializerInterceptor implements NestInterceptor {
   constructor(private dto: any) {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,13 +7,13 @@ import {
   Patch,
   Post,
   Query,
-  UseInterceptors,
 } from '@nestjs/common';
-import { SerializerInterceptor } from '../interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserDto } from './dto/user.dto';
 import { UsersService } from './users.service';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -28,7 +28,6 @@ export class UsersController {
     return this.usersService.find(email);
   }
 
-  @UseInterceptors(new SerializerInterceptor(UserDto))
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
