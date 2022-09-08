c5a95c6434dc63c80ec66fddd81d2d4092ffe8be
Erik S. Carlsten <ecarlste@gmail.com>
2022-09-08 17:19:37 -0600

    Added test for user controller signin

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -27,7 +27,9 @@ describe('UsersController', () => {
 
     fakeAuthService = {
       // signup: () => Promise.resolve(),
-      // signin: () => Promise.resolve(),
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -66,5 +68,16 @@ describe('UsersController', () => {
     await expect(controller.findUser('1')).rejects.toThrowError(
       NotFoundException,
     );
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin(
+      { email: 'asdf@asdf.com', password: 'asdf' },
+      session,
+    );
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
