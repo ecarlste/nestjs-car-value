cf4de71e5ba887b6ef86ef965f9a89f4804e2594
Erik S. Carlsten <ecarlste@gmail.com>
2022-09-12 13:18:33 -0600

    Added a basic test around signup and whoami

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -29,4 +29,22 @@ describe('Auth System', () => {
         expect(email).toEqual(email);
       });
   });
+
+  it('signup as a new user and then get the current user', async () => {
+    const email = 'asdf@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
