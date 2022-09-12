5fb497e7c1873306aa8990ed8c4c4dfb42e49087
Erik S. Carlsten <ecarlste@gmail.com>
2022-09-12 14:19:49 -0600

    Wired up create report flow

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../guards/auth.guard';
 import { CreateReportDto } from './dto/create-report.dto';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
 export class ReportsController {
+  constructor(private readonly reportsService: ReportsService) {}
+
   @Post()
+  @UseGuards(AuthGuard)
   createReport(@Body() body: CreateReportDto) {
-    return body;
+    return this.reportsService.create(body);
   }
 }
