6651a5d3401daa2ac01cb76bef6e5beb4ff33f1d
Erik S. Carlsten <ecarlste@gmail.com>
2022-09-12 15:39:14 -0600

    Added use of AdminGuard

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -6,14 +6,15 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
+import { AdminGuard } from '../guards/admin.guard';
+import { AuthGuard } from '../guards/auth.guard';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
-import { AuthGuard } from '../guards/auth.guard';
-import { CreateReportDto } from './dto/create-report.dto';
-import { ReportsService } from './reports.service';
-import { Serialize } from '../interceptors/serialize.interceptor';
-import { ReportDto } from './dto/report.dto';
 import { ApproveReportDto } from './dto/approve-report.dto';
+import { CreateReportDto } from './dto/create-report.dto';
+import { ReportDto } from './dto/report.dto';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
 export class ReportsController {
@@ -27,6 +28,7 @@ export class ReportsController {
   }
 
   @Patch('/:id')
+  @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.updateApproval(id, body.approved);
   }
