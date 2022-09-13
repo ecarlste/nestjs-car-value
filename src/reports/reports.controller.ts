137821a254e8a6a7e02ef13065c39f0c49d8919f
Erik S. Carlsten <ecarlste@gmail.com>
2022-09-13 16:55:40 -0600

    Added logic for determining average price estimate in with criteria

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -32,7 +32,7 @@ export class ReportsController {
 
   @Get()
   getEstimate(@Query() query: GetEstimateDto) {
-    console.log(query);
+    return this.reportsService.getEstimate(query);
   }
 
   @Patch('/:id')
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,8 +1,9 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from '../users/user.entity';
 import { Repository } from 'typeorm';
+import { User } from '../users/user.entity';
 import { CreateReportDto } from './dto/create-report.dto';
+import { GetEstimateDto } from './dto/get-estimate.dto';
 import { Report } from './report.entity';
 
 @Injectable()
@@ -16,6 +17,22 @@ export class ReportsService {
     report.user = user;
 
     return this.repo.save(report);
+  }
+
+  getEstimate({ make, model, year, mileage, lng, lat }: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
   }
 
   async updateApproval(id: string, approved: boolean) {
