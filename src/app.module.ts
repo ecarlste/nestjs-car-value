7fe2ce496f2ca23e5bc19dc71f5bf063f89cc5c8
Erik S. Carlsten <ecarlsten@statestitle.com>
2023-03-31 22:36:03 -0600

    Added COOKIE_KEY env var use

--- src/app.module.ts
+++ src/app.module.ts
@@ -37,7 +37,10 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(cookieSession({ keys: ['asdfasdf'] })).forRoutes('*');
+    consumer
+      .apply(cookieSession({ keys: [this.configService.get('COOKIE_KEY')] }))
+      .forRoutes('*');
   }
 }
