95234c1f2d8f3d1aed79327f077de96fa695179d
Erik S. Carlsten <ecarlsten@statestitle.com>
2023-03-31 22:19:30 -0600

    Added some initial fakes to controller and service unit tests

--- src/reports/reports.service.spec.ts
+++ src/reports/reports.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReportsService } from './reports.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Report } from './report.entity';
 
 describe('ReportsService', () => {
   let service: ReportsService;
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const fakeReportsRepository = () => {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReportsService],
+      providers: [
+        ReportsService,
+        {
+          provide: getRepositoryToken(Report),
+          useFactory: fakeReportsRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ReportsService>(ReportsService);
